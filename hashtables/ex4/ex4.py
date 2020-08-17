56787e1cad69e93458f2caaba7c239d82a3a4597
Kevin <kevincarr632@gmail.com>
2020-08-16 20:48:26 -0500

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,19 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    integers = dict()
+    result = []
 
+    for num in a:
+         # if the num has a corresponding number in the dict
+        if integers.get(abs(num)):
+            # check if they add to 0
+            if (integers.get(abs(num)) + num) == 0:
+                result.append(abs(num))
+
+        else:
+             # if no value is found, pass num as new key along with it's value
+            integers[abs(num)] = num
     return result
 
 
